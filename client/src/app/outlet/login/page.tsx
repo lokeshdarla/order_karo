8323c8a6a4ab09be06f68c9a6714f91b223c3188
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-04-20 06:29:15 +0530

    [client] website flow

--- client/src/app/outlet/login/page.tsx
+++ client/src/app/outlet/login/page.tsx
@@ -42,7 +42,7 @@ const Page: FC = () => {
             alt="SRM AP Logo"
           />
           UniCart
-        </a>
+        </a >
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -123,8 +123,8 @@ const Page: FC = () => {
             </form>
           </div>
         </div>
-      </div>
-    </section>
+      </div >
+    </section >
   );
 };
 
